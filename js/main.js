abbcedb2c0b58d6a2f53c05f618ab72ee8a9e4c0
Javier <jlalovi@gmail.com>
2013-11-27 19:24:25 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -12,15 +12,15 @@ function changeTab(){
 }
 
 function increaseLikes(){
-  var myString = $(this).children('nlike').first().html(); // Note: http://stackoverflow.com/questions/306583/this-selector-and-children
+  var myString = $(this).children('.nLikes').first().html(); // Note: http://stackoverflow.com/questions/306583/this-selector-and-children
   var myInteger = parseInt(myString); // Note: http://www.minihowtos.net/jquery-parseint
   var myNewInteger = myInteger + 1;
   var nlikesUp = myNewInteger.toString(); // changing again to an string
 
-  $(this, '.nLikes').html(nlikesUp);
+  $(this).children('.nLikes').first().html(nlikesUp);
 }
 
 $('#tab-navigation li').click(changeTab);
-$('.likes').click(nlikesUp);
+$('.likes').click(increaseLikes);
 
 });
